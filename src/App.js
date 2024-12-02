4dfe8dffa39f0f7e770bdd401108bd8b580c9761
Lauri Hynninen <laurihyn@gmail.com>
2024-12-02 14:32:35 +0200

    Deployment stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import CustomerList from './CustomerList'; // Asiakaslistakomponentti
 import TrainingList from './TrainingList'; // Harjoitusten listakomponentti
 import TrainingCalendar from './TrainingCalendar'; // Kalenterikomponentti
